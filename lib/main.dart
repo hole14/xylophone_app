e6a9cdeddc08a1c756a125d7858cfc931233261f
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-05-06 13:37:15 +0700

    Menyelesaikan aplikasi xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,43 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+
+  void playSound(int soundNumber){
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildKey(Color color, int soundNumber){
+    return Expanded(
+      child: TextButton(
+        onPressed: (){
+          playSound(soundNumber);
+        },
+        child: Container(
+          color: color,
+          height: 100.0,
+        )
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-                onPressed: (){
-                  final player = AudioPlayer();
-                  player.play(AssetSource('note1.wav'));
-                },
-                child: Text("Click Me")),
+          child: Column(
+            children: <Widget>[
+              buildKey(Colors.teal[300]!, 1),
+              buildKey(Colors.teal[400]!, 2),
+              buildKey(Colors.teal[500]!, 3),
+              buildKey(Colors.teal[600]!, 4),
+              buildKey(Colors.teal[700]!, 5),
+              buildKey(Colors.teal[800]!, 6),
+              buildKey(Colors.teal[900]!, 7),
+            ],
           ),
         ),
       ),
